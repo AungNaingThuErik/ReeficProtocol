ec08a4af9afcd11bc1af2a68812dde2718643c43
KoAungNaingThu <aungnaingthu2727@gmail.com>
2021-04-08 16:25:21 +0800

    Updated Media Query

--- script.js
+++ script.js
@@ -24,9 +24,6 @@
 })();
 
 $("#carousel-example").on("slide.bs.carousel", function (e) {
-  /*
-      CC 2.0 License Iatek LLC 2018 - Attribution required
-  */
   var $e = $(e.relatedTarget);
   var idx = $e.index();
   var itemsPerSlide = 5;
@@ -35,7 +32,6 @@ $("#carousel-example").on("slide.bs.carousel", function (e) {
   if (idx >= totalItems - (itemsPerSlide - 1)) {
     var it = itemsPerSlide - (totalItems - idx);
     for (var i = 0; i < it; i++) {
-      // append slides to end
       if (e.direction == "left") {
         $(".carousel-item").eq(i).appendTo(".carousel-inner");
       } else {
